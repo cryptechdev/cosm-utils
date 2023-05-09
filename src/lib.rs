83ee36b6df7a7aadf023ee10daf3117e0db0410e
Eric Woolsey <ewoolsey@ualberta.ca>
2023-05-08 20:13:26 -0700

    pub use

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub mod signing_key;
 
 pub mod chain;
 
+pub use tendermint_rpc;
+
 pub mod prelude {
     pub use crate::clients::client::ClientUtils;
     pub use crate::modules::{auth::api::Auth, bank::api::Bank, cosmwasm::api::Cosmwasm};
